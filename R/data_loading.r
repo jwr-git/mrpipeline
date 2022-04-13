28a570d4a93a509ee869b7b91a7bcd910af2a02f
jwr-git <jwr-git@outlook.com>
2022-04-13 17:18:20 +0100

    Allows for reading of bgz vcf files

--- R/data_loading.r
+++ R/data_loading.r
@@ -384,7 +384,7 @@ read_outcome <- function(ids,
     id <- ids[i]
 
     # Extract data from local GWAS VCF format file
-    if (file.exists(id) && tools::file_ext(id) == "vcf")
+    if (file.exists(id) && (tools::file_ext(id) == "vcf" || tools::file_ext(id) == "bgz"))
     {
       .print_msg(paste0("Reading \"", id, "\" as GWAS VCF file."), verbose)
 
