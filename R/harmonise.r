abf41b8d55345af1eaf4751076f7c19ef723b88e
jwr-git <jwr-git@outlook.com>
2022-07-22 13:01:56 +0100

    Adds `pairwise_analysis` and updates documentation

--- R/harmonise.r
+++ R/harmonise.r
@@ -9,6 +9,7 @@
 #' @param action How to harmonise alleles; see \link[TwoSampleMR]{harmonise_data}.
 #' @param cores Number of cores for multi-threaded tasks (Optional)
 #'              NB: Unavailable on Windows machines
+#' @param verbose Display verbose information (Optional, boolean)
 #'
 #' @return Harmonised data.frame
 #' @importFrom TwoSampleMR harmonise_data
@@ -70,4 +71,74 @@ is_palindromic <- function(A1, A2)
 is_ambiguous <- function(freq, tol = 0.08)
 {
   freq > 0.5 - tol & freq < 0.5 + tol
+}
+
+#' Performs pairwise harmonisation and analyses -- helpful when analysing many
+#' exposure-outcome pairs as performing the standard "linear" approach will
+#' be very slow.
+#'
+#' In this function, an exposure-outcome pair are harmonised, analyses are ran
+#' on those data and those results are saved to a file. Analyses ran can be MR
+#' or colocalisation, as desired.
+#'
+#' @param exposure Data.frame of exposure dataset(s)
+#' @param outcome Data.frame of outcome dataset(s)
+#' @param res_path Path to save result files
+#' @param ... Other arguments for the following functions:
+#'            \link[mrpipeline]{harmonise}
+#'            \link[mrpipeline]{do_mr}
+#'            \link[mrpipeline]{do_coloc}
+#' @param do_coloc True/False run colocalisation analyses
+#' @param cores Number of cores for multi-threaded tasks (Optional)
+#'              NB: Unavailable on Windows machines
+#' @param verbose Display verbose information (Optional, boolean)
+#'
+#' @importFrom tidyr crossing
+#' @importFrom parallel mclapply
+#'
+#' @export
+pairwise_analysis <- function(exposure,
+                              outcome,
+                              res_path,
+                              ...,
+                              do_coloc = FALSE,
+                              cores = 1,
+                              verbose = TRUE)
+{
+  if (!dir.exists(res_path)) {
+    stop("Pathway does not exist: \"", res_path, "\".")
+  }
+  pairs <- tidyr::crossing(exposure$id.exposure, outcomes$id.outcome)
+  names(pairs) <- c("id.exposure", "id.outcome")
+  parallel::mclapply(1:nrow(pairs), function(i)
+  {
+    exp <- exposure[exposure$id.exposure == pairs[i, "id.exposure"][[1]], ]
+    out <- outcome[outcome$id.outcome == pairs[i, "id.outcome"][[1]], ]
+
+    dat <- harmonise(exp, out, ..., cores, verbose)
+
+    mr_res <- do_mr(dat, ..., verbose = verbose)
+    write.table(mr_res,
+                file = paste0(res_path,
+                              "/",
+                              pairs[i, "id.exposure"][[1]],
+                              "_",
+                              pairs[i, "id.outcome"][[1]],
+                              "_mr_results.txt"),
+                ...)
+
+    if (do_coloc)
+    {
+      coloc_res <- do_coloc(dat, ..., cores = cores, verbose = verbose)
+      write.table(coloc_res,
+                  file = paste0(res_path,
+                                "/",
+                                pairs[i, "id.exposure"][[1]],
+                                "_",
+                                pairs[i, "id.outcome"][[1]],
+                                "_coloc_results.txt"),
+                  ...)
+    }
+
+  }, mc.cores = cores)
 }
